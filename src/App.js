7144b86ed5ae1627102d2692efd40978c5bcccfe
User <user@Users-MacBook-Pro.local>
2020-07-21 16:20:13 +0300

    create timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  timerId;
+
+  state = {
+    name: '',
+    count: 0,
+    initialCount: 0,
+  }
+
+
+  startTimer = () => {
+
+    this.setState({
+      count: this.state.count - 1,
+    })
+
+    if (this.state.count >= 0) {
+      this.timerId = setTimeout(() => {
+        this.startTimer();
+      }, 1000);
+    } else {
+      clearTimeout(this.timerId);
+    }
+  }
+
+  stopTimer = () => {
+    clearTimeout(this.timerId);
+  }
+
+  restartTimer = () => {
+    clearTimeout(this.timerId);
+    this.setState({
+      count: this.state.initialCount,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      name: e.target.timername.value,
+      count: e.target.timercount.value,
+      initialCount: e.target.timercount.value,
+    })
+
+    e.target.timername.value = '';
+    e.target.timercount.value = '';
+  }
+  render() {
+
+    return (
+      <div>ROOT
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Timer name:
+          <input type="text" name="timername" />
+          </label>
+          <label>
+            Initial count:
+          <input type="text" name="timercount" />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+
+        {
+          this.state.count > 0 &&
+          <div>
+            <p>{this.state.name}</p>
+            <button onClick={this.startTimer}>Start</button>
+            <button onClick={this.stopTimer}>Stop</button>
+            <button onClick={this.restartTimer}>Restart</button>
+            <p>{this.state.count}</p>
+          </div>
+        }
+
+        {
+          !this.state.count && this.state.initialCount > 0 && <div>Your time is over!!!!!!</div>
+        }
+        
+      </div>
+    )
+  }
 }
 
 export default App;
